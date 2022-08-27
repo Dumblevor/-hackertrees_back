a65068d36022512dbc834641aa53b858ad6d1421
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-08-27 18:30:54 +0300

    fixes1

--- config/environment.js
+++ config/environment.js
@@ -8,16 +8,10 @@ export const secret = process.env.SECRET || 'growTreesNoChopChopGottaSustain'
 
 
 
-//old ver
-
 // const environment = process.env.NODE_ENV
-
 // const mongoURL = "mongodb://127.0.0.1:27017/"
-
 // export const dbURL = environment === 'test'
 //   ? `${mongoURL}hackertrees-test`
 //   : `${mongoURL}hackertrees`
-
-// //secret for jwt
 // export const secret = "growTreesNoChopChopGottaSustain"
 
